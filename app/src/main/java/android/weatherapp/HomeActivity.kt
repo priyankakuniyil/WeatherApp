95d7d820631a4fec4d1c3ca0a4262da7290cef10
Priyanka <priyankakuniyil@gmail.com>
2019-12-26 11:43:47 +0530

    Layout Design (1.home page 2. no recent search items 3. auto complete list item )

---
+++ app/src/main/java/android/weatherapp/HomeActivity.kt
@@ -0,0 +1,12 @@
+package android.weatherapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
